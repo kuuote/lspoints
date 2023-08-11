7ba52c0df90b884e2c9444a5a3949b2dfa9a8a2c
kuuote <znmxodq1@gmail.com>
2023-08-11 23:54:39 +0900

    こんなもんか

---
+++ denops/@lspoints/box.ts
@@ -0,0 +1,25 @@
+import {
+  deepMerge,
+  DeepMergeOptions,
+} from "https://deno.land/std@0.198.0/collections/deep_merge.ts";
+
+export class PatchableObjectBox<T extends Record<PropertyKey, unknown>> {
+  #store: T;
+  constructor(defaultValue: T) {
+    this.#store = defaultValue;
+  }
+
+  get(): T {
+    return this.#store;
+  }
+
+  set(value: T) {
+    this.#store = value;
+  }
+
+  patch(value: Partial<T>, options: DeepMergeOptions = {
+    arrays: "replace",
+  }) {
+    this.#store = deepMerge<T>(this.#store, value, options);
+  }
+}
